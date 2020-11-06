a51d958dc615b25df9d8461c55351b10160ac0f2
ajevans <ajecoder@gmail.com>
2020-11-06 15:34:42 -0500

    add ui event handler

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,3 +3,36 @@ const signUpSuccess = function (res) {
   $('#status-message').text('Sign up successful')
   $('#sign-up-form').trigger('reset')
 }
+const signUpFailure = function (error) {
+  $('#status-message').text('Sign up failed, try again')
+}
+const logInSuccess = function (res) {
+  $('#status-message').text('Sign in successful')
+  $('#sign-in-form').trigger('reset')
+}
+const logInFailure = function (error) {
+  $('#status-message').text('Sign in failed, try again')
+}
+const changePWSuccess = function (res) {
+  $('#status-message').text('Password change successful')
+  $('#change-pw-form').trigger('reset')
+}
+const changePWFailure = function (error) {
+  $('#status-message').text('Password change failed, try again')
+}
+const logOutSuccess = function (res) {
+  $('#status-message').text('Sign out successful')
+}
+const logOutFailure = function (error) {
+  $('#status-message').text('Sign out failed, try again')
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  logInSuccess,
+  logInFailure,
+  changePWSuccess,
+  changePWFailure,
+  logOutSuccess,
+  logOutFailure
+}
