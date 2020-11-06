58e505addd20caaa3e968f16204088c19a08f352
ajevans <ajecoder@gmail.com>
2020-11-06 11:00:05 -0500

    add auth event handlers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,11 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
+const authEvents = require('./auth/events')
 $(() => {
   // your JS code goes here
+$('#sign-up-form').on('submit', authEvents.signUp)
+$('#sign-in-form').on('submit', authEvents.logIn)
+$('#change-pw-form').on('submit', authEvents.changePW)
+$('#sign-out-form').on('submit', authEvents.logOut)
 })
