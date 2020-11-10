db69f4d006128ff9910c0998caf29b4d1543d3ab
ajevans <ajecoder@gmail.com>
2020-11-10 15:39:47 -0500

    add auth event handlers

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,25 +1,41 @@
 'use strict'
 const formFields = require('./../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
 
 // signup
 const signUp = (event) => {
   event.preventDefault()
   const form = event.target
   const data = formFields(form)
+  // const data = formFields.getFormFields(form)
+  console.log(data)
+  api.signUp(data)
+  .then(ui.signUpSuccess)
+  .catch(ui.signUpFailure)
 
 }
 const logIn = (event) => {
   event.preventDefault()
   const form = event.target
   const data = formFields(form)
+  api.logIn(data)
+  .then(ui.logInSuccess)
+  .catch(ui.logInFailure)
 }
 const changePW = (event) => {
   event.preventDefault()
   const form = event.target
   const data = formFields(form)
+  api.changePW(data)
+  .then(ui.changePWSuccess)
+  .catch(ui.changePWFailure)
 }
 const logOut = (event) => {
   event.preventDefault()
+  api.logOut(data)
+  .then(ui.logOutSuccess)
+  .catch(ui.logOutFailure)
 }
 module.exports = {
   signUp,
