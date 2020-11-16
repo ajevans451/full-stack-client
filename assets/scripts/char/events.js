2f6fc3b98966fcb342a966cdcadb2b086d154510
ajevans <ajecoder@gmail.com>
2020-11-16 16:40:38 -0500

    add char api functions

--- assets/scripts/char/events.js
+++ assets/scripts/char/events.js
@@ -1,23 +1,30 @@
 'use strict'
 const formFields = require('./../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+const store = require('./../store')
 
 const createCharacter = (event) => {
   event.preventDefault()
   const form = event.target
   const data = formFields(form)
+  api.createChar()
 }
 const indexCharacter = (event) => {
   event.preventDefault()
   const form = event.target
   const data = formFields(form)
+  api.charIndex()
 }
 const updateCharacter = (event) => {
   event.preventDefault()
   const form = event.target
   const data = formFields(form)
+  api.updateChar()
 }
 const deleteCharacter = (event) => {
   event.preventDefault()
+  api.deleteChar()
 }
 module.exports = {
   createCharacter,
